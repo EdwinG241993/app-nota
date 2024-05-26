e9e7219f3f1da50a26ee35bbd8ecf944ed19faab
EdwinGEscobar1993 <edwing24@gmail.com>
2024-05-26 00:36:29 -0500

    Module note

--- app.js
+++ app.js
@@ -2,15 +2,25 @@ import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import path from 'path';
-
+const mongoose = require('mongoose');
 const app = express();
+
+const uri = 'mongodb://localhost:27017/myapp';
+
+// Or using promises
+mongoose.connect(uri).then(
+  /** ready to use. The `mongoose.connect()` promise resolves to mongoose instance. */
+  () => { console.log('Conectado a DB') },
+  /** handle initial connection error */
+  err => { console.log(err) }
+);
 
 // Middleware
 app.use(morgan('tiny'));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use('/api', require('./routes/nota'));
 
 // Rutas
 app.get('/', (req, res) => {
@@ -24,5 +34,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('puerto', process.env.PORT || 3000);
 app.listen(app.get('puerto'), () => {
-  console.log('Example app listening on port'+ app.get('puerto'));
+  console.log('Example app listening on port' + app.get('puerto'));
 });
